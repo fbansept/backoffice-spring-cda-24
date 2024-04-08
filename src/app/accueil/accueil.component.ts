9bcf134652bec335cde5bf49912b342c4d9e2e17
fbansept <bansept.franck@gmail.com>
2024-04-08 08:46:39 +0200

    JWT

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrl: './accueil.component.scss',
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+  http: HttpClient = inject(HttpClient);
 
+  listeProduit: any[] = [];
+
+  ngOnInit(): void {
+    this.http
+      .get<any[]>('http://localhost:8080/produit/liste')
+      .subscribe((listeProduit) => (this.listeProduit = listeProduit));
+  }
+
+  onSupprimerProduit(idProduit: number): void {
+    this.http
+      .delete('http://localhost:8080/produit/' + idProduit)
+      .subscribe((resultat) => console.log(resultat));
+  }
 }
--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   templateUrl: './connexion.component.html',
-  styleUrl: './connexion.component.scss'
+  styleUrl: './connexion.component.scss',
 })
 export class ConnexionComponent {
+  formBuilder: FormBuilder = inject(FormBuilder);
+  http: HttpClient = inject(HttpClient);
+  router: Router = inject(Router);
 
+  formulaire: FormGroup = this.formBuilder.group({
+    email: ['a@a.com', [Validators.email, Validators.required]],
+    motDePasse: ['root', [Validators.required]],
+  });
+
+  onConnexion() {
+    if (this.formulaire.valid) {
+      this.http
+        .post<{ jwt: string }>(
+          'http://localhost:8080/connexion',
+          this.formulaire.value
+        )
+        .subscribe((resultat) => {
+          localStorage.setItem('jwt', resultat.jwt);
+          this.router.navigateByUrl('/accueil');
+        });
+    }
+  }
 }

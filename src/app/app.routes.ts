df74f252e2aaff532c3ddc67c68720f87b983538
fbansept <bansept.franck@gmail.com>
2024-04-11 17:09:55 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { PageNonTrouveComponent } from './page-non-trouve/page-non-trouve.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { EditProduitComponent } from './edit-produit/edit-produit.component';
 
 export const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'connexion', component: ConnexionComponent },
   { path: 'inscription', component: InscriptionComponent },
+  { path: 'edit-produit', component: EditProduitComponent },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: '**', component: PageNonTrouveComponent },
 ];

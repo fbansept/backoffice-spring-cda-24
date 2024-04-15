57954d39b9862407c7766ad36d0a4b775dee0bf3
fbansept <bansept.franck@gmail.com>
2024-04-15 15:01:26 +0200

    edit produit

--- src/app/edit-produit/edit-produit.component.ts
+++ src/app/edit-produit/edit-produit.component.ts
@@ -1,42 +1,57 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-edit-produit',
   standalone: true,
   imports: [
-    MatButtonModule, MatSelectModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-produit.component.html',
-  styleUrl: './edit-produit.component.scss'
+  styleUrl: './edit-produit.component.scss',
 })
-export class EditProduitComponent implements OnInit{
-  
-  http: HttpClient = inject(HttpClient)
-  formBuilder: FormBuilder = inject(FormBuilder)
-  formulaire: FormGroup = this.formBuilder.group(
-    {
-      nom:['',[Validators.required]],
-      code:['',[Validators.required]],
-      description:['',[]],
-      prix:[1,[]],
-      etat:[null,[Validators.required]],
-      listeEtiquettes:[[],[]]
-    }
-  )
+export class EditProduitComponent implements OnInit {
+  http: HttpClient = inject(HttpClient);
+  formBuilder: FormBuilder = inject(FormBuilder);
+  formulaire: FormGroup = this.formBuilder.group({
+    nom: ['', [Validators.required]],
+    code: ['', [Validators.required]],
+    description: ['', []],
+    prix: [1, []],
+    etat: [null, [Validators.required]],
+    listeEtiquettes: [[], []],
+  });
 
   listeEtat: any[] = [];
+  listeEtiquettes: any[] = [];
 
   ngOnInit(): void {
     this.http
-      .get<any[]>("http://localhost:8080/etat-produit/liste")
-      .subscribe(resultat => this.listeEtat = resultat)
+      .get<any[]>('http://localhost:8080/etat-produit/liste')
+      .subscribe((resultat) => (this.listeEtat = resultat));
 
+    this.http
+      .get<any[]>('http://localhost:8080/etiquette-produit/liste')
+      .subscribe((resultat) => (this.listeEtiquettes = resultat));
   }
 
+  onSubmit() {
+    console.log(this.formulaire.value);
+  }
 }
